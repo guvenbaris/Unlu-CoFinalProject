43c6c2bc9578246164037c4e382a86ee0ec8e3a0
Güven Barış ÇAKAN <gvnbrs54@gmail.com>
2022-03-11 20:18:34 +0300

    Project files updated.

---
+++ UnluCoProductCatalog.Application/ViewModels/OfferViewModels/UpdateOfferViewModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace UnluCoProductCatalog.Application.ViewModels.OfferViewModels
+{
+    public class UpdateOfferViewModel
+    {
+        public int PercentRate { get; set; }
+        public double OfferedPrice { get; set; }
+        public int ProductId { get; set; }
+    }
+}
---
+++ UnluCoProductCatalog.Persistence/Services/RabbitMQ/RabbitMqService.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+using UnluCoProductCatalog.Application.Interfaces.RabbitMQ;
+
+namespace UnluCoProductCatalog.Persistence.Services.RabbitMQ
+{
+    public class RabbitMqService : IRabbitMqService
+    {
+
+        public IConnection GetRabbitMqConnection()
+        {
+            ConnectionFactory connectionFactory = new()
+            {
+                HostName = "localhost"
+            };
+
+            return connectionFactory.CreateConnection();
+        }
+    }
+
+}
